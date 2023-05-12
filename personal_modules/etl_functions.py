4f54c3304490861380cbdecff68b3f13b12645a8
DanielDavila <dadavilal@correo.udistrital.edu.co>
2023-05-11 20:37:11 -0500

    fix list_outer_value function

--- personal_modules/etl_functions.py
+++ personal_modules/etl_functions.py
@@ -17,4 +17,4 @@ def list_outer_values(df_origin,column_pk,df_join,column_fk):
     """Extract unique values from 2 dataframes and returns outer values from df_join"""
     l1= list(df_origin[column_pk].unique()) #Extract list of unique values from pk table
     l2=list(df_join[column_fk].unique()) # Extract listo of unique values from fk table
-    return list(set(l2) ^ set(l1))
+    return [x for x in l2 if x not in l1]
