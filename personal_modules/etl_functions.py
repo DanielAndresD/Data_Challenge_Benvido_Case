b8e71ce55003a388fb92217f0ed758287767398a
DanielDavila <dadavilal@correo.udistrital.edu.co>
2023-05-11 19:12:33 -0500

    add etl file includes Extract process

--- personal_modules/etl_functions.py
+++ personal_modules/etl_functions.py
@@ -12,3 +12,9 @@ def extract_data(csv_path: str):
         return df 
     except Error as ex:
         print("Error during file extract: {}".format(ex))
+
+def list_outer_values(df_origin,column_pk,df_join,column_fk):
+    """Extract unique values from 2 dataframes and returns outer values from df_join"""
+    l1= list(df_origin[column_pk].unique()) #Extract list of unique values from pk table
+    l2=list(df_join[column_fk].unique()) # Extract listo of unique values from fk table
+    return list(set(l2) ^ set(l1))
